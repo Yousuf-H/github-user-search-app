80020f51dd7127e92f39ee91dc11949111561281
Yousuf-Hassan <yousuf.hassan2021@gmail.com>
2021-12-13 18:16:17 +1300

    delete console.log

--- src/API/githubAPI.js
+++ src/API/githubAPI.js
@@ -3,6 +3,5 @@ import { publicRequest } from './requestMethod'
 // Get User data from git hub endpoint
 export const getUserData = async (username) => {
   const res = await publicRequest.get(username)
-  console.log(res.data)
   return res.data
 }
